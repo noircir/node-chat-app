84a446132970dfa65e6ccfd34ff3e3cf2284c151
Nellie K <noircir@users.noreply.github.com>
2018-09-15 18:27:35 -0400

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,8 +15,32 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 // register an event listener to listen for connection
+// it is like a tunnel 
 io.on('connection', (socket) => {
 	console.log('NEW USER CONNECTED');
+
+	socket.emit('newEmail', {
+		from: "mike@example.com",
+		text: "Hey, what is going on.",
+		createdAt: 123
+	});
+
+	// event listener for when client sends an email
+	socket.on('createEmail', (newEmail) => {
+		console.log('Client sent email: ', newEmail);
+	});
+
+	// pushing SMS
+	socket.emit('newMessage', {
+		from: "sigourney",
+		text: "What's up?",
+		createdAt: 123789
+	});
+
+	// event listener for when client sends an SMS
+	socket.on('createMessage', (sms) => {
+		console.log('Client sent message: ', sms);
+	});
 
 	socket.on('disconnect', (socket) => {
 		console.log('USER DISCONNECTED');
