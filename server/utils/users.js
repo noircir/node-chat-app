72f5192c716b7a591cd238c4aeeb88844595e62c
Nellie K <noircir@users.noreply.github.com>
2018-09-21 20:16:19 -0400

    Add methods to Users class and add user tests

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,22 +1,33 @@
-[{
-	id: 'lakjfdjf',
-	name: 'Nellie',
-	room: 'The Office Fans'
-}]
-
-// addUser(id, name, room)
-//removeUser(id)
-// getUser(id)
-// getUserList(room)
-
 class Users {
 	constructor () {
 		this.users = [];
 	}
-	addUsers (id, name, room) {
+
+	addUser (id, name, room) {
 		var user = {id, name, room};
 		this.users.push(user);
 		return user;
+	}
+
+	removeUser (id) {
+		var user = this.getUser(id);
+
+		if (user) {
+			this.users = this.users.filter((user) => user.id !== id);
+		}
+		return user;
+	}
+
+	getUser (id) {
+		var user = this.users.filter((user) => user.id === id);
+		return user[0];
+	}
+
+	getUserList (room) {
+		var users = this.users.filter((user) => user.room === room);
+		var namesArray = users.map((user) => user.name); 
+
+		return namesArray;
 	}
 }
 
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,7 +2,25 @@ const expect = require('expect');
 
 const {Users} = require('./users');
 
+beforeEach(() => {
+	chatUsers = new Users();
+	chatUsers.users = [{
+		id: '1',
+		name: 'Mike',
+		room: 'Node Course'
+	}, {
+		id: '2',
+		name: 'Sigourney',
+		room: 'React Course'
+	}, {
+		id: '3',
+		name: 'Dmitri',
+		room: 'Node Course'
+	}];
+});
+
 describe('Users', () => {
+
 	it('should add new user', () => {
 		var chatUsers = new Users();
 		var user = {
@@ -10,9 +28,57 @@ describe('Users', () => {
 			name: 'Nellie',
 			room: 'The Office Fans'
 		}
-		expect(chatUsers.addUsers(user.id, user.name, user.room)).toEqual(user);
+		expect(chatUsers.addUser(user.id, user.name, user.room)).toEqual(user);
 
 		// verifying that constructor has created an array with one element
 		expect(chatUsers.users).toEqual([user]);
 	});
+
+
+	it('should return names for Node course', () => {
+		var userList = chatUsers.getUserList('Node Course');
+
+		expect(userList).toEqual(['Mike', 'Dmitri']);
+	});
+
+
+	it('should return names for React course', () => {
+		var userList = chatUsers.getUserList('React Course');
+
+		expect(userList).toEqual(['Sigourney']);
+	});
+
+
+	it('should remove a user', () => {
+		var removedUser = chatUsers.removeUser('1');
+
+		expect(removedUser.id).toBe('1');
+		expect(chatUsers.users.length).toBe(2);
+	});
+
+
+	it('should not remove a user', () => {
+		var user = chatUsers.removeUser('123');
+
+		expect(user).toBeFalsy();
+		expect(chatUsers.users.length).toBe(3);
+	});
+
+
+	it('should find user', () => {
+		var userToFind = chatUsers.getUser('2');
+
+		expect(userToFind).toEqual({
+			id: '2',
+			name: 'Sigourney',
+			room: 'React Course'
+		});
+	});
+
+	it('should not find user', () => {
+		var userToFind = chatUsers.getUser('24');
+
+		expect(userToFind).not.toBeDefined();
+	});
+
 });
